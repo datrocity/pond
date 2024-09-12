b725b0d6802de737908f6e7443afab4823b8cbac
Pietro Berkes <pietro.berkes@googlemail.com>
2024-09-12 08:43:48 +0200

    Matplotlib figure creates a PNG artifact

--- pond/artifact/pil_image_artifact.py
+++ pond/artifact/pil_image_artifact.py
@@ -6,9 +6,39 @@ from pond.artifact import Artifact
 from pond.artifact.artifact_registry import global_artifact_registry
 
 
+def _image_from_matplotlib_fig(fig):
+    """ Transform a Matplotlib Figure in a PIL RGB Image object"""
+    fig.canvas.draw()
+    image = Image.frombytes(
+        'RGB',
+        fig.canvas.get_width_height(),
+        fig.canvas.tostring_rgb()
+    )
+    return image
+
+
 class PILImageArtifact(Artifact):
     """ Artifact for Matlab figures.
     """
+
+    def __init__(self, data, metadata=None, data_hash=None):
+        """ Create an Artifact.
+
+        Parameters
+        ----------
+        data: Figure
+            A Matplotlib Figure object. It is going to be transformed in a PNG figure.
+        metadata: dict
+            User-defined metadata, saved with the artifact (optional).
+            The metadata keys and values will be stored as strings.
+        data_hash: str
+            The data hash of the data, if known (for example, when the artifact
+            is read from a Version). If None, the hash is re-computed.
+        """
+        # Transform the matplotlib figure in a PNG image.
+        if isinstance(data, Figure):
+            data = _image_from_matplotlib_fig(data)
+        super().__init__(data, metadata, data_hash=data_hash)
 
     # --- Artifact class interface
 
@@ -24,18 +54,6 @@ class PILImageArtifact(Artifact):
     def filename(basename):
         return basename + '.png'
 
-    # --- PILImageArtifact class interface
-
-    @classmethod
-    def from_matplotlib_fig(cls, fig, metadata=None):
-        fig.canvas.draw()
-        image = Image.frombytes(
-            'RGB',
-            fig.canvas.get_width_height(),
-            fig.canvas.tostring_rgb()
-        )
-        return cls(image, metadata)
-
     # --- Artifact interface
 
     def write_bytes(self, file_, **kwargs):
@@ -46,27 +64,6 @@ class PILImageArtifact(Artifact):
         self.data.save(file_, pnginfo=png_metadata)
 
 
-class MatplotlibFigureArtifact(Artifact):
-
-    # --- Artifact class interface
-
-    # TODO: this can't return another class
-    @classmethod
-    def _read_bytes(cls, file_, **kwargs):
-        """ Read a Matlab figure artifact from CSV file. """
-        image = Image.open(file_).copy()
-        metadata = image.info
-        return PILImageArtifact(image, metadata)
-
-    @staticmethod
-    def filename(basename):
-        return basename + '.png'
-
-    def write_bytes(self, file_, **kwargs):
-        pil = PILImageArtifact.from_matplotlib_fig(self.data, self.metadata)
-        pil.write_bytes(file_, **kwargs)
-
-
 global_artifact_registry.register(artifact_class=PILImageArtifact, data_class=Image, format='png')
-global_artifact_registry.register(artifact_class=MatplotlibFigureArtifact, data_class=Figure,
+global_artifact_registry.register(artifact_class=PILImageArtifact, data_class=Figure,
                                   format='png')
