7efcb68604061509df996506b97d9a0f5dffabb8
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2025-03-25 20:05:32 +0100

    uncompressed

---
+++ pond/artifact/numpy_array_artifact_uncompressed.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from pond.artifact import Artifact
+from pond.artifact.artifact_registry import global_artifact_registry
+
+
+class NumpyArrayArtifactUncompressed(Artifact):
+    """ Artifact for numpy arrays.
+
+    The array is saved in .npz format. This format allows to store numpy arrays without installing an additional
+    dependency, and to include the metadata as an additional array in the file itself.
+    """
+
+    @classmethod
+    def _read_bytes(cls, file_, **kwargs):
+        """ Read a numpy array from a npy binary file. """
+        content = np.load(file_, allow_pickle=True)
+        data = content['data']
+        metadata = content['metadata'][()]
+        return data, metadata
+
+    def write_bytes(self, file_):
+        # The pond convention is that all stored metadata is a string
+        #metadata = {k: str(v) for k, v in self.metadata.items()}
+        #metadata_array = np.array(metadata)
+        np.save(file_, self.data)
+
+    @staticmethod
+    def filename(basename):
+        return basename + '.npy'
+
+
+global_artifact_registry.register(artifact_class=NumpyArrayArtifactUncompressed, data_class=np.ndarray, format='npy')
