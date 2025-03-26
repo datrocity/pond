5fa737ff953b7bca98e13dd75c655c554765f59c
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2025-03-26 10:10:20 +0100

    make memmap artefact

--- pond/artifact/numpy_array_artifact_mmap.py
+++ pond/artifact/numpy_array_artifact_mmap.py
@@ -4,7 +4,7 @@ from pond.artifact import Artifact
 from pond.artifact.artifact_registry import global_artifact_registry
 
 
-class NumpyArrayArtifactUncompressed(Artifact):
+class NumpyArrayArtifactMmap(Artifact):
     """ Artifact for numpy arrays.
 
     The array is saved in .npz format. This format allows to store numpy arrays without installing an additional
@@ -14,15 +14,20 @@ class NumpyArrayArtifactUncompressed(Artifact):
     @classmethod
     def _read_bytes(cls, file_, **kwargs):
         """ Read a numpy array from a npy binary file. """
-        content = np.load(file_, allow_pickle=True)
-        data = content['data']
-        metadata = content['metadata'][()]
+
+        data = np.load(file_.name, allow_pickle=True, mmap_mode="r")
+
+        # how to load the metadata here???
+        #data = content['data']
+        #metadata = content['metadata'][()]
+        metadata = None
         return data, metadata
 
     def write_bytes(self, file_):
         # The pond convention is that all stored metadata is a string
-        #metadata = {k: str(v) for k, v in self.metadata.items()}
-        #metadata_array = np.array(metadata)
+        metadata = {k: str(v) for k, v in self.metadata.items()}
+        metadata_array = np.array(metadata)
+        # how to save the metadata here???
         np.save(file_, self.data)
 
     @staticmethod
@@ -30,4 +35,4 @@ class NumpyArrayArtifactUncompressed(Artifact):
         return basename + '.npy'
 
 
-global_artifact_registry.register(artifact_class=NumpyArrayArtifactUncompressed, data_class=np.ndarray, format='npy')
+global_artifact_registry.register(artifact_class=NumpyArrayArtifactMmap, data_class=np.ndarray, format='npy_mmap')
